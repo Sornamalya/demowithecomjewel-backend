f463e71231b0947ee7ddf71f9741595112415a23
Sornamalya L <sornamalya08@gmail.com>
2025-11-21 02:17:49 +0530

    Enable CORS for frontend

--- src/main/java/com/example/demowithecomjewel/config/WebConfig.java
+++ src/main/java/com/example/demowithecomjewel/config/WebConfig.java
@@ -11,13 +11,14 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:uploads/");
-                  
     }
-//     @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("http://localhost:5174")
-//                .allowedMethods("*");
-//    }
-	 
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://customised-jewels.netlify.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
